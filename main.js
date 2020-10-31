33a0e410b6e45dd1b2d4fe57b7769f81163b4650
Shaun Gibb <sgibb1@mail.valenciacollege.edu>
2020-10-31 00:06:51 -0400

    Completed JS and HTML. Added CSS.

--- main.js
+++ main.js
@@ -1,36 +1,194 @@
-// BUTTONS ON THE PAGE
-const numberButtons = document.querySelectorAll('.number');
-const operatorButtons = document.querySelectorAll('.operator');
-const equalButton = document.querySelector('.equal');
-const clearButton = document.querySelector('.clear');
+$(document).ready(function () {
 
-// TODO make references to all the proper HTML elements you'll be using to display elements to the user
+  var calcStr = ""; // main display
+  var histStr = ""; // history display
+  var total = ""; // running total
+  var visible = false; //main display
+  var dpoint_active = true; // able to create decimal
+  var oper_active = true; // able to use operator
 
-// TODO make variables to keep track of the 1st number, operator, 2nd number, and the result of the math.
+  //get data when calculator key is clicked
+  document.getElementById('keypad').addEventListener('click', getKey);
 
-for (let i = 0; i < numberButtons.length; i++) {
-  numberButtons[i].addEventListener('click', clickNumber);
-}
-for (let i = 0; i < operatorButtons.length; i++) {
-  operatorButtons[i].addEventListener('click', clickOperator);
-}
-equalButton.addEventListener('click', clickEqualButton);
-clearButton.addEventListener('click', clickClearButton);
+  function getKey(k) {
+    var key = k.target.id;
+    switch (key) {
+      case 'zero': key = 0;
+        break;
+      case 'one': key = 1;
+        break;
+      case 'two': key = 2;
+        break;
+      case 'three': key = 3;
+        break;
+      case 'four': key = 4;
+        break;
+      case 'five': key = 5;
+        break;
+      case 'six': key = 6;
+        break;
+      case 'seven': key = 7;
+        break;
+      case 'eight': key = 8;
+        break;
+      case 'nine': key = 9;
+        break;
+    }
+    compute(key);
+  }
 
-function clickNumber(event) {
-  console.log(event.target.value);
-  // CODE GOES HERE
-}
+  //display and calulate
+  function displayIt(display, history) {
+    document.getElementById("display").innerHTML = display;
+    document.getElementById("history").innerHTML = history;
+  }
 
-function clickOperator(event) {
-  console.log(event.target.value);
-  // CODE GOES HERE
-}
+  function calcIt(show, func) {
+    calcStr += show;
+    histStr += show;
+    total += func;
+    displayIt(calcStr, histStr);
+  }
 
-function clickEqualButton() {
-  // CODE GOES HERE
-}
+  function deleteOne() {
+    calcStr = calcStr.slice(0, -1);
+    histStr = histStr.slice(0, -1);
+    total = total.slice(0, -1);
+    displayIt(calcStr, histStr);
+  }
 
-function clickClearButton() {
-  // CODE GOES HERE
-}
+  function compute(data) {
+    //NUMBERS
+    if (calcStr === "" && data === 0) {
+      return 0;
+    }
+
+    if (data >= 0 && data <= 9) {
+      if (visible) {
+        calcStr = "";
+        histStr = ""
+        total = "";
+        visible = false;
+      }
+
+      oper_active = true;
+      calcIt(data, data);
+    }
+
+    //DECIMAL
+    if (data == "decimal") {
+      if (dpoint_active === false) {
+        return 0;
+      }
+      if (visible || calcStr == 0 || histStr == 0) {
+        calcStr = "0.";
+        histStr = "0.";
+        total = "0.";
+        visible = false;
+        displayIt(calcStr, histStr);
+        dpoint_active = false;
+      } else {
+        calcIt(".", ".");
+        dpoint_active = false;
+      }
+    }
+
+    //OPERATORS
+    if (data == "add") {
+      if (total == "") {
+        return 0;
+      }
+      if (oper_active == false) {
+        deleteOne();
+      }
+      visible = false;
+      dpoint_active = true;
+      calcIt("+", "+");
+      oper_active = false;
+    }
+    if (data == "subtract") {
+      if (total == "") {
+        return 0;
+      }
+      if (oper_active == false) {
+        deleteOne();
+      }
+      visible = false;
+      dpoint_active = true;
+      calcIt("-", "-");
+      oper_active = false;
+    }
+    if (data == "multiply") {
+      if (total == "") {
+        return 0;
+      }
+      if (oper_active == false) {
+        deleteOne();
+      }
+      visible = false;
+      dpoint_active = true;
+      oper_active = false;
+      calcIt("x", "*");
+    }
+    if (data == "divide") {
+      if (total == "") {
+        return 0;
+      }
+      if (oper_active == false) {
+        deleteOne();
+      }
+      visible = false;
+      dpoint_active = true;
+      oper_active = false;
+      calcIt("/", "/");
+    }
+
+    //EQUALS
+    if (data == "equals") {
+      if (total == "" || visible) {
+        return 0;
+      }
+      visible = true;
+      dpoint_active = true;
+      histStr += " = \xa0" + eval(total);
+      displayIt(eval(total), histStr);
+      total = document.getElementById("display").innerHTML;
+      calcStr = total;
+    }
+
+    //CLEAR
+    if (data == "clear") {
+      calcStr = "";
+      histStr = "";
+      total = "";
+      visible = false;
+      dpoint_active = true;
+      displayIt("0", "0");
+    }
+
+    //DELETE
+    if (data == "delete") {
+      if (visible == true || calcStr == "" || calcStr.length == 1) {
+        calcStr = "0";
+        histStr = "0";
+        displayIt(calcStr, histStr);
+        return 0;
+      }
+
+      deleteOne();
+
+    }
+    //ERRORS
+
+    if (calcStr.length > 25 || total == "NaN") {
+      displayIt("Error", "0");
+      return 0;
+    }
+    console.log("***")
+    console.log(`Display ${calcStr}`);
+    console.log(`History ${histStr}`);
+    console.log(`Total ${total}`);
+    console.log("---");
+
+  }
+});
